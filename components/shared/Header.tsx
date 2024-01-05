6e24100a8baa4b446b2b58169544a883f283d899
duyguca <duygukoksalca@gmail.com>
2024-01-05 14:33:52 -0800

    feat:mobile navbar and footer added

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,10 +1,11 @@
-import { SignIn, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
 import { Button } from "../ui/button";
+import NavItems from "./NavItems";
+import MobileNav from "./MobileNav";
 
-function Header() {
+const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
@@ -16,9 +17,17 @@ function Header() {
             alt="Evently logo"
           />
         </Link>
+
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
+
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/" />
+            <MobileNav />
           </SignedIn>
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
@@ -29,6 +38,6 @@ function Header() {
       </div>
     </header>
   );
-}
+};
 
 export default Header;
